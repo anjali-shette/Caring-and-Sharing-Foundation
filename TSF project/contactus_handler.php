2278790c27813c3559479bb15d15b1944ef8aea9
anjali-shette <85779762+anjali-shette@users.noreply.github.com>
2021-07-15 18:58:50 +0530

    Add files via upload

---
+++ TSF project/contactus_handler.php
@@ -0,0 +1,24 @@
+<?php
+	$name = $_POST['name'];
+	$visitor_email = $_POST['email'];
+	$message = $_POST['message'];
+	
+	$email_from='anjalishette19@gmail.com';
+	
+	$email_subject="New Form submission";
+	
+	$email_body="User Name: $name.\n".
+				"User Email: $visitor_email.\n".
+				"User Message: $message.\n";
+				
+	$to="anjalishette19@gmail";
+	
+	$headers="form: $email_from\r\n";
+	
+	$headers .="Reply-To: $visitor_email\r\n";
+	
+	mail($to,$email_subject,$email_body,$headers);
+	
+	header("Location: contact_us.html");
+	
+	?>
